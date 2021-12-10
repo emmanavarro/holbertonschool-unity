2503028152b4ebd7a05ad6030edc34b8fbdae000
emmanavarro <elnavarro55@gmail.com>
2021-12-10 02:08:50 -0500

    Assets added

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/LinksButtons.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinksButtons : MonoBehaviour
+{
+  public void Twitter()
+  {
+    Application.OpenURL("https://twitter.com/ayy_emma");
+  }
+  public void Github()
+  {
+    Application.OpenURL("https://github.com/emmanavarro");
+  }
+  public void Email()
+  {
+    Application.OpenURL("mailto:elnavarro55@gmail.com");
+  }
+  public void LinkedIn()
+  {
+    Application.OpenURL("https://www.linkedin.com/in/emmanavarromillan/");
+  }
+}
